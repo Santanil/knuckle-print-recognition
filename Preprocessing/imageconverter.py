7b463d5ffbece596bab8f9114ec63d87cae04d53
Bikram Modak <bikrammodak75@gmail.com>
2018-12-29 23:29:07 +0530

    KPR : Libraries changed. -Use of Scikit-image ,OpenCV, matplotlib -Grayscale conversion changed -Addition of noise -Blur detection and blur addition -Removal of noise -Utilities :Saving images

--- Preprocessing/imageconverter.py
+++ Preprocessing/imageconverter.py
@@ -1,62 +1,88 @@
 AUTHOR = "BIKRAM MODAK"
-# from PIL import Image
+
 from glob import glob
 from scipy import ndimage,misc
 import numpy as np
 import matplotlib.pyplot as plt
 
-print('_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_')
-print("|              IMAGE CONVERTER STARTED                |")
-print('*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*')
-
-# other setup variables will be added later
-with open("setup.config",'r') as setupfile:
-    imageDir = setupfile.read()
-
-print("\n\nLoading... ",end='')
-print(imageDir)
+# For blur detection
+from imutils import paths
+import cv2
 
 class ImageConverter:
     # Memory to store images to be preprocessed
     imageQueue=[]
     def __init__(self,imageDir):
+        print('*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_')
+        print('|              IMAGE CONVERTER EXECUTING              ')
+        print('*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_')
         self.m_imagelist = glob(imageDir+"*")
 
     def getOriginalImages(self):
         # glob function finds files with the given pattern here is the string in image_directory
         for imageName in self.m_imagelist:
             m_image = ndimage.imread(imageName)
-            # print(m_image.shape,type(m_image))
+            # print("Image Shape:",m_image.shape,type(m_image))
             ImageConverter.imageQueue.append(m_image)
 
     def convertToGrayScale(self):
         # glob function finds files with the given pattern here is the string in image_directory
         for imageName in self.m_imagelist:
-            m_image = ndimage.imread(imageName,mode = 'L')
-            # print(m_image.shape,type(m_image))
-            ImageConverter.imageQueue.append(m_image)
+            m_image = cv2.imread(imageName)
+            m_gray_image = cv2.cvtColor(m_image, cv2.COLOR_BGR2GRAY)
+            # print("Image Shape after grayscale conversion:",m_gray_image.shape,type(m_gray_image))
+            ImageConverter.imageQueue.append(m_gray_image)
 
-    
+    def add_noise(self):
+        m_temporaryImageList = []
+        m_temporaryImageList[:] = ImageConverter.imageQueue[:]
+        ImageConverter.imageQueue[:] = []
+        for imagearray in m_temporaryImageList:
+            m_noisy_image = imagearray + 0.4 * imagearray.std() * np.random.random(imagearray.shape)
+            ImageConverter.imageQueue.append(m_noisy_image)
+        del m_temporaryImageList
+
+    def de_noise(self):
+        m_temporaryImageList = []
+        m_temporaryImageList[:] = ImageConverter.imageQueue[:]
+        ImageConverter.imageQueue[:] = []
+        for noisy in m_temporaryImageList:
+            denoised_image = ndimage.median_filter(noisy,2)
+            ImageConverter.imageQueue.append(denoised_image)
+        del m_temporaryImageList
+
+    def detectBlur(self,image):
+        # compute the Laplacian of the image and then return the focus
+        # measure, which is simply the variance of the Laplacian
+        return cv2.Laplacian(image, cv2.CV_64F).var()
+
+    def addBlur(self):
+        m_temporaryImageList = []
+        m_temporaryImageList[:] = ImageConverter.imageQueue[:]
+        ImageConverter.imageQueue[:] = []
+        for image in m_temporaryImageList:
+            very_blurred = ndimage.gaussian_filter(image,sigma=5)
+            ImageConverter.imageQueue.append(very_blurred)
+        del m_temporaryImageList
+
+    def getBlurValue(self):
+        for imagearray in ImageConverter.imageQueue:
+            blur_value = self.detectBlur(imagearray)
+            print("--Blur Value (less is more) :", blur_value)
 
     @staticmethod
     def showImageQueue():
-        print(ImageConverter.imageQueue)
+        for i in ImageConverter.imageQueue:
+            print(i)
 
     @staticmethod
-    def showImage():
+    def showImage(imagetitle):
         for imagearray in ImageConverter.imageQueue:
+            plt.title(imagetitle)
             plt.imshow(imagearray,cmap=plt.cm.gray)
             plt.show()
 
     @staticmethod
     def saveImage():
         for index,imagearray in enumerate(ImageConverter.imageQueue):
-            misc.imsave("{0}_new.jpg".format(index),imagearray)
-        
-
-
-imageConverter = ImageConverter(imageDir)
-imageConverter.getOriginalImages()
-imageConverter.convertToGrayScale()
-ImageConverter.showImageQueue()
-ImageConverter.showImage()
+            misc.imsave("saved_images\\{0}_new.jpg".format(index),imagearray)
--- app.py
+++ app.py
@@ -1,2 +1,45 @@
 AUTHOR = "BIKRAM MODAK"
 print("***************** WELCOME TO KNUCKLE PRINT RECOGNITION SYSTEM *****************")
+import argparse
+from Preprocessing.imageconverter import ImageConverter
+
+
+# other setup variables will be added later
+with open("setup.config",'r') as setupfile:
+    imageDir = setupfile.read()
+
+print("\n\nLoading... ",end='')
+print(imageDir)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--convert_to_grayscale", help="convert images to grayscale", action="store_true")
+parser.add_argument("--add_noise", help="add noise to the images", action="store_true")
+parser.add_argument("--add_blur", help="add noise to the images", action="store_true")
+parser.add_argument("--de_noise", help="de noising/smoothing of images", action="store_true")
+parser.add_argument("--show_images", help="show images in different window", action="store_true")
+parser.add_argument("--save_images", help="save images in a different folder", action="store_true")
+
+args = parser.parse_args()
+
+imageConverter = ImageConverter(imageDir)
+# imageConverter.getOriginalImages()
+if args.convert_to_grayscale:
+    imageConverter.convertToGrayScale()
+    # ImageConverter.showImageQueue()
+if args.add_noise:
+    imageConverter.add_noise()
+    imageConverter.getBlurValue()
+    ImageConverter.showImage("After adding noise getting blur value")
+    # ImageConverter.showImageQueue()
+if args.add_blur:
+    imageConverter.addBlur()
+    imageConverter.getBlurValue()
+    ImageConverter.showImage("After adding blur getting blur value")
+if args.de_noise:
+    imageConverter.de_noise()
+    imageConverter.getBlurValue()
+    # ImageConverter.showImageQueue()
+if args.show_images:
+    ImageConverter.showImage("After denoising getting blur value")
+if args.save_images:
+    ImageConverter.saveImage()
